847d507e62afcce5edd0c00c6d994cb08af57310
Katharina Dietz <kdietz@uni-muenster.de>
2025-12-05 09:19:53 +0100

    Add simple assertion tests.

--- src/test/java/de/ulbms/scdh/spike/git/PalindromeTest.java
+++ src/test/java/de/ulbms/scdh/spike/git/PalindromeTest.java
@@ -1,4 +1,23 @@
 package de.ulbms.scdh.spike.git;
 
-public class PalindromeTest {
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+class PalindromeTest {
+    @Test
+    void testIsPalindrome(){
+        assertTrue(Palindrome.isPalindrome("uhu"));
+    }
+
+    @Test
+    void testMixedCases(){
+       assertEquals("uhu", Palindrome.lowerCase("Uhu"));
+    }
+
+    @Test
+    void testSpaces(){
+        assertEquals("uhuuhu", Palindrome.removeSpaces("uhu uhu"));
+    }
+
 }
